0becb458eadadeb928ece44af0aeec369d168baa
Tomharv <240168801@aston.ac.uk>
2025-11-23 22:09:34 +0000

    Added 'create_cart_items_table' migration file

---
+++ clothes-shop/database/migrations/2025_11_23_215801_create_cart_items_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->unsignedBigInteger('cart_id');
+            $table->unsignedBigInteger('variant_id');
+            $table->integer('qty');
+            $table->timestamps();
+
+            //  Constraints
+
+            /**
+             *  Create composite key (consits of cart_id and variant_id)
+             */
+
+            $table->primary(['cart_id', 'variant_id']);
+            
+            /**
+             *  cart_id foreign key constraint
+             */
+
+            $table->foreign('cart_id')->references('cart_id')
+            ->on('carts')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+
+            /**
+             *  variant_id foreign key constraint
+             */
+
+            $table->foreign('variant_id')->references('variant_id')
+            ->on('product_variants')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
