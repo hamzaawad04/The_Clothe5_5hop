22eac411678921307c8891e4ea0443c023343375
Hamza Awad <hamzaawad04@outlook.com>
2025-11-24 19:19:02 +0000

    Added code to confirm connection to database

--- clothes-shop/resources/views/welcome.blade.php
+++ clothes-shop/resources/views/welcome.blade.php
@@ -273,5 +273,14 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <strong>Database Connected: </strong>
+<?php
+    try {
+        \DB::connection()->getPDO();
+        echo \DB::connection()->getDatabaseName();
+        } catch (\Exception $e) {
+        echo 'None';
+    }
+?>
     </body>
 </html>
