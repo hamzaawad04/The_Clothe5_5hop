6290765318e1646728e0c534b433b80d96afb975
Tomharv <240168801@aston.ac.uk>
2025-11-25 16:40:06 +0000

    Added 'create_contact_messages_table' migration file

---
+++ clothes-shop/database/migrations/2025_11_25_162937_create_contact_messages_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_messages', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->text('message');
+            $table->enum('status', ['new', 'in_progress', 'closed']);
+            $table->timestamps();
+
+            //  Constraints
+
+            /**
+             *  Index (idx_contact_messages_user (user_id))
+             */
+
+            $table->index(['user_id'], 'idx_contact_messages_user');
+
+
+            /**
+             *  Foreign Key (user_id)
+             */
+
+            $table->foreign('user_id')->references('user_id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_messages');
+    }
+};
