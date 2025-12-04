9c4ca4490874434502a49a1f5d7c0446c56e1e49
Ma afee Basimahgit config --global user.email 230248896@aston.ac.uk <230248896@aston.ac.uk>
2025-12-04 18:52:05 +0000

    Fixed bottoms link in navbar

--- clothes-shop/resources/views/components/mainnavbar.blade.php
+++ clothes-shop/resources/views/components/mainnavbar.blade.php
@@ -48,7 +48,7 @@
      <!-- Categories -->
    <nav class="flex justify-between px-20 py-3 text-lg font-medium">
        <a href="{{ route('tops') }}" class="hover:text-cathover">Tops</a>
-       <a href="#" class="hover:text-cathover">Bottoms</a>
+       <a href="{{ route('bottoms') }}" class="hover:text-cathover">Bottoms</a>
        <a href="{{ route('footwear') }}" class="hover:text-cathover">Footwear</a>
        <a href="#" class="hover:text-cathover">Outerwear</a>
        <a href="#" class="hover:text-cathover">Accessories</a>
