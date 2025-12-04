261eae3b57e7bc633c992ea56bad038fb7b83548
Hamza Awad <hamzaawad04@outlook.com>
2025-12-04 15:42:46 +0000

    Completed CartController and its routes

--- clothes-shop/routes/web.php
+++ clothes-shop/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ContactMessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
     return view('pages.home');
@@ -14,9 +15,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    /* Profile routes */
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    /* Cart routes */
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.basket');
+    Route::post('/cart/add', [CartController::class, 'addItem'])->name('cart.add');
+    Route::post('/cart/update/{variant_id}', [CartController::class, 'updateQuantity'])->name('cart.update');
+    Route::delete('/cart/remove/{variant_id}', [CartController::class, 'removeItem'])->name('cart.remove');
+    Route::delete('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 });
 
 Route::get('/checkout', [OrderController::class, 'showCheckout'])
@@ -27,13 +35,8 @@ Route::post('/checkout', [OrderController::class, 'checkout'])
     ->middleware('auth')
     ->name('checkout.place-order');
 
-Route::get('/about', function () {
-    return view('pages.about');
-})->name('about');
-
 Route::get('/contact', [ContactMessageController::class, 'create'])->name('contact');
 Route::post('/contact', [ContactMessageController::class, 'store'])->name('contact.store');
-
 
 Route::get('/about', function () {
     return view('pages.about');
