b48e8b5f2b67898398023d3e3768975793bb32f4
Hamza Awad <hamzaawad04@outlook.com>
2025-12-05 11:32:41 +0000

    Added titles to relevant pages

--- clothes-shop/resources/views/products/index.blade.php
+++ clothes-shop/resources/views/products/index.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>THE CLOTHE5 5HOP – Tops</title>
+  <title>Products</title>
   
   <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
