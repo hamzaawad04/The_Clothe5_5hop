23fecf1b89b21608d57702053f8dba92c0e54e80
uzairaston <240229870@aston.ac.uk>
2025-12-05 01:09:07 +0000

    updated homepage with more banners

--- clothes-shop/resources/views/pages/home.blade.php
+++ clothes-shop/resources/views/pages/home.blade.php
@@ -20,8 +20,41 @@
     @include('components.mainnavbar')
 
     <main>
-        <img src="{{ asset('images/herobanner1.png') }}" alt="Model Logo" class="logo" style="width: 100%; height: auto;"> 
-    
+        <div class="relative w-full">
+        <img src="{{ asset('images/herobanner1.png') }}" alt="Model Logo" class="w-full h-auto">
+
+        <a href="{{ route('about') }}" class="absolute bottom-[22%] left-[14%] px-8 py-4 border border-cathover text-3xl text-white rounded-lg hover:text-cathover">About Us</a>
+        </div>
+
+        <div class="relative w-full">
+        <img src="{{ asset('images/herobannertops.png') }}" alt="Model Logo" class="w-full h-auto">
+
+        <a href="{{ route('products.tops') }}" class="absolute bottom-[22%] right-[14%] px-8 py-4 border border-cathover text-3xl text-white rounded-lg hover:text-cathover">Tops</a>
+        </div>
+
+        <div class="relative w-full">
+        <img src="{{ asset('images/herobannerbottoms.png') }}" alt="Model Logo" class="w-full h-auto">
+
+        <a href="{{ route('products.bottoms') }}" class="absolute bottom-[22%] left-[14%] px-8 py-4 border border-cathover text-3xl text-white rounded-lg hover:text-cathover">Bottoms</a>
+        </div>
+
+        <div class="relative w-full">
+        <img src="{{ asset('images/herobannerouterwear.png') }}" alt="Model Logo" class="w-full h-auto">
+
+        <a href="{{ route('products.outerwear') }}" class="absolute bottom-[22%] right-[14%] px-8 py-4 border border-cathover text-3xl text-white rounded-lg hover:text-cathover">Outerwear</a>
+        </div>
+
+        <div class="relative w-full">
+        <img src="{{ asset('images/herobannerfootwear.png') }}" alt="Model Logo" class="w-full h-auto">
+
+        <a href="{{ route('products.footwear') }}" class="absolute bottom-[22%] left-[14%] px-8 py-4 border border-cathover text-3xl text-white rounded-lg hover:text-cathover">Footwear</a>
+        </div>
+
+        <div class="relative w-full">
+        <img src="{{ asset('images/herobanneraccessories.png') }}" alt="Model Logo" class="w-full h-auto">
+
+        <a href="{{ route('products.accessories') }}" class="absolute bottom-[22%] right-[14%] px-8 py-4 border border-cathover text-3xl text-white rounded-lg hover:text-cathover">Accessories</a>
+        </div>
     </main>
      @include('components.footer')
 </body>
